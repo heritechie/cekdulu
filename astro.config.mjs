b9a380778b302f9100510fee35dae0979a5da0d5
heritechie <heritechie@gmail.com>
2026-08-31 21:49:50 +0700

    polish calculator wizard and SEO

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import tailwindcss from '@tailwindcss/vite';
 import react from '@astrojs/react';
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://cekdulu.co.id',
   vite: {
     plugins: [tailwindcss()]
   },
